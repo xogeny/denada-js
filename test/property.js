0ecf884cb437345163fda82224f43e3ea76061f4
Michael Tiller <michael.tiller@gmail.com>
2014-03-24 16:08:54 -0400

    Oops, forgot some files

---
+++ test/property.js
@@ -0,0 +1,24 @@
+var denada = require('../denada');
+var assert = require('assert');
+
+describe("Property example", function() {
+    it("Example code should parse", function (done) {
+	var tree = denada.parseFileSync("test/samples/property.dnd");
+	assert.equal(tree.length, 4);
+	done();
+    });
+    it("Grammar should parse", function (done) {
+	var rules = denada.parseFileSync("test/samples/propertyGrammar.dnd");
+	assert.equal(rules.length, 3);
+	done();
+    });
+    it("Example code should follow grammar", function (done) {
+	var tree = denada.parseFileSync("test/samples/property.dnd");
+	var rules = denada.parseFileSync("test/samples/propertyGrammar.dnd");
+	var issues = denada.process(tree, rules);
+
+	for(var i=0;i<issues.length;i++) console.log("Unexpected issue: "+issues[i]);
+	assert.equal(issues.length, 0);
+	done();
+    });
+});
