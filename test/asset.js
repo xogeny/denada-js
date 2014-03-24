8fe363f8e2edfbeb480f7db6e032ed014ec5f953
Michael Tiller <michael.tiller@gmail.com>
2014-03-24 16:42:43 -0400

    Added asset example

---
+++ test/asset.js
@@ -0,0 +1,24 @@
+var denada = require('../denada');
+var assert = require('assert');
+
+describe("Asset example", function() {
+    it("Example code should parse", function (done) {
+	var tree = denada.parseFileSync("test/samples/assets.dnd");
+	assert.equal(tree.length, 3);
+	done();
+    });
+    it("Grammar should parse", function (done) {
+	var rules = denada.parseFileSync("test/samples/assetGrammar.dnd");
+	assert.equal(rules.length, 2);
+	done();
+    });
+    it("Example code should follow grammar", function (done) {
+	var tree = denada.parseFileSync("test/samples/assets.dnd");
+	var rules = denada.parseFileSync("test/samples/assetGrammar.dnd");
+	var issues = denada.process(tree, rules);
+
+	for(var i=0;i<issues.length;i++) console.log("Unexpected issue: "+issues[i]);
+	assert.equal(issues.length, 0);
+	done();
+    });
+});
