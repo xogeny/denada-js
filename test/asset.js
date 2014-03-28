ac90282eb383d8b3170e0060a991c6c264630848
Michael Tiller <michael.tiller@gmail.com>
2014-03-27 21:38:21 -0400

    Added a bunch of tests that should generate issues but not initially

--- test/asset.js
+++ test/asset.js
@@ -9,7 +9,7 @@ describe("Asset example", function() {
     });
     it("Grammar should parse", function (done) {
 	var rules = denada.parseFileSync("test/samples/assetGrammar.dnd");
-	assert.equal(rules.length, 2);
+	assert.equal(rules.length, 3);
 	done();
     });
     it("Example code should follow grammar", function (done) {
@@ -19,6 +19,33 @@ describe("Asset example", function() {
 
 	for(var i=0;i<issues.length;i++) console.log("Unexpected issue: "+issues[i]);
 	assert.equal(issues.length, 0);
+	done();
+    });
+});
+
+describe("Asset errors", function() {
+    it("should detect non-matching definitions", function(done) {
+	var tree = denada.parseFileSync("test/samples/asset_error1.dnd");
+	var rules = denada.parseFileSync("test/samples/assetGrammar.dnd");
+	var issues = denada.process(tree, rules);
+
+	assert.equal(issues.length, 1);
+	done();
+    });
+    it("should detect mismatched assignment types", function(done) {
+	var tree = denada.parseFileSync("test/samples/asset_error2.dnd");
+	var rules = denada.parseFileSync("test/samples/assetGrammar.dnd");
+	var issues = denada.process(tree, rules);
+
+	assert.equal(issues.length, 2);
+	done();
+    });
+    it("should detect invalid inner elements", function(done) {
+	var tree = denada.parseFileSync("test/samples/asset_error3.dnd");
+	var rules = denada.parseFileSync("test/samples/assetGrammar.dnd");
+	var issues = denada.process(tree, rules);
+
+	assert.equal(issues.length, 1);
 	done();
     });
 });
